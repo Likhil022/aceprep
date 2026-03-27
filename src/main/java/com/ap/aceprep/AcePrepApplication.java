de65295f4e7c58cb3cca37c6e105e21f8b8f09d2
Likhil022 <likhilkosuru123@gmail.com>
2026-03-27 10:42:04 +0530

    ace prep

---
+++ src/main/java/com/ap/aceprep/AcePrepApplication.java
@@ -0,0 +1,13 @@
+package com.ap.aceprep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcePrepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcePrepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ap/aceprep/AcePrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ap.aceprep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcePrepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
